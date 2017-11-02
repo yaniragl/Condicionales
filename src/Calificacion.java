b4eba581f6ffe51d7b9bdf6edc5c76db11f2f65c
yaniragl <yanira_galeas@hotmail.com>
2017-11-02 20:49:26 -0300

    Programa de calificacion de promedio de notas alumno

---
+++ src/Calificacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ceduc
+ */
+public class Calificacion 
+
+{
+     public static void main(String[] args)
+{
+float nota1, nota2, nota3;
+nota1 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
+nota2 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota"));
+nota3 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota"));
+
+}
+     
+         
+     }
+     
+         
+     
