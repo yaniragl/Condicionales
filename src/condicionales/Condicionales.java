99c5149ba587004801097ffac11a3da4458d66a9
yaniragl <yanira_galeas@hotmail.com>
2017-11-02 19:48:15 -0300

    Operador de comparacion > en el if y else anidados

--- src/condicionales/Condicionales.java
+++ src/condicionales/Condicionales.java
@@ -29,8 +29,18 @@ public class Condicionales {
         }
       else//Si no se cumple la condicion se ejecuta esto
         {
-            
+            JOptionPane.showMessageDialog(null, "son Diferentes"); 
+            if ((numero1 > numero2)) 
+            {
+              JOptionPane.showMessageDialog(null, "Numero" + numero1+"  es mayor"); 
+                     
+            }
+            else
+            {
+              JOptionPane.showMessageDialog(null, "Numero" + numero2+" es mayor");  
+            }
         }
+        
     }
         
     
