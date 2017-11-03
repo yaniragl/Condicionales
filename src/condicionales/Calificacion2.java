6603a35f1f47887b9ef15a7cb854623f0ca6ac5d
yaniragl <yanira_galeas@hotmail.com>
2017-11-03 19:27:37 -0300

    Calificacion por ventana

---
+++ src/condicionales/Calificacion2.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ceduc
+ */
+public class Calificacion2 
+{
+   public static void main(String[] args)
+   {  
+     float nota;
+     
+nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese  una nota"));
+
+if (nota ==7)
+{ 
+    JOptionPane.showMessageDialog(null, "notable" );       
+{    
+      else  
+    {
+                     
+   if(nota==6){
+               
+       JOptionPane.showMessageDialog(null,"Bien" );
+          
+       }
+   else
+       
+   }  
+       if(nota> && notas<5)
+           {
+            JOptionPane.showMessageDialog(null,"Insuficiente" );
+           
+           }
+            
+}
+
+    
+   
+   
+       
+   }
+       
+   }
+       
+   }
+
+
+
