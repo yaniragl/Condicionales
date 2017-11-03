d6747f5a1425d8cf32efe63304ce673237180f9f
yaniragl <yanira_galeas@hotmail.com>
2017-11-02 21:25:34 -0300

    Promedio de notas por ventana

--- src/Calificacion.java
+++ src/Calificacion.java
@@ -8,20 +8,34 @@ import javax.swing.JOptionPane;
  *
  * @author ceduc
  */
-public class Calificacion 
+public class Calificacion {
 
-{
+
      public static void main(String[] args)
-{
-float nota1, nota2, nota3;
-nota1 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
-nota2 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota"));
-nota3 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota"));
+          
+     {
+float nota1, nota2, nota3,promedio = 0;
 
-}
-     
+nota1 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota 1"));
+nota2 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota 2"));
+nota3 =  Float.parseFloat(JOptionPane.showInputDialog("Ingrese otra nota 3"));
+promedio= (nota1+nota2+nota3)/3;
+
+if (promedio > 4)
+{
+     JOptionPane.showMessageDialog(null, "Aprobado" ); 
          
      }
+else
+{
+    
+    JOptionPane.showMessageDialog(null, "Reprobado");
+}
      
-         
-     
+      
+    
+}
+    
+}
+    
+
